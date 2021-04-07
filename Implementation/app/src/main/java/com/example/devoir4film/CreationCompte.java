2ff796b6bb3e522e2d31fff9feb7521e85266881
LauraSenegas <laurasenegas@hotmail.com>
2021-04-07 12:13:58 -0400

    Page "Création de compte" est fini
    (le bouton "créer le compte" est activé)

---
+++ Implementation/app/src/main/java/com/example/devoir4film/CreationCompte.java
@@ -0,0 +1,27 @@
+package com.example.devoir4film;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CreationCompte extends AppCompatActivity {
+    Button btCreerCompte;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creation_compte);
+
+        // Lorsque l'utilisateur clique sur le bouton « Créer le comte », il est amené vers la page « choisirHumeur »
+        final Button button = findViewById(R.id.btCreerCompte);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(CreationCompte.this, choisirHumeur.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
